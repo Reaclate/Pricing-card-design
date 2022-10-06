b68c5a166bd3a5307578f07f2afed8492ee8056f
Tinsaye Simeneh <tinsayesimeneh608@gmail.com>
2022-10-06 14:46:03 +0300

    simple card structure

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.css';
+import Card from './components/Card';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <Card />
+);
+
